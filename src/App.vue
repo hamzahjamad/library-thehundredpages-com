7c44e1bd412c0da16db4ff5c7ad46602de20f87d
Hamzah Jamad <hamzahjamad@gmail.com>
2019-06-14 21:24:25 +0800

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="row">
+    <div class="col-12" v-for="post in postWithLink" :key="post.id">
+      <BookReview :collections="post"></BookReview>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookReview from './components/BookReview.vue'
+import axios from 'axios'
+import _ from 'lodash';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  computed: {
+      postWithLink() {
+         let postCollection = this.posts.map(p => {
+           let temp = {
+            id: '',
+            title : '',
+            link : '',
+            first_letter: ''
+           };
+
+           temp.id = p.id;
+           temp.title = p.title.rendered.replace(/[A-Za-z\W]+\W+[\||:]\W+/g, ""); //remove 'Book Review | ' keyword from title
+           temp.link = p.link;
+           temp.first_letter = temp.title.substring(0,1);
+
+           return temp;
+         }).sort((a, b) => (a.title > b.title) ? 1 : -1);
+
+         return _(postCollection)
+                  .groupBy(x => x.first_letter)
+                  .map((value, key) => ({first_letter: key, posts: value}))
+                  .value();
+      } 
+  },
+  methods: {
+     populatePosts() {
+       let query = ( ids => {
+        ids = JSON.parse(ids);
+        let result = "?per_page=100&";
+        for (let i = ids.length - 1; i >= 0; i--) {
+          result += "categories=" + ids[i]
+          if (i !== 0) {
+            result += ",";
+          }
+        }
+
+        return result;
+     })(process.env.VUE_APP_CATEGORY_IDS);
+
+     //TODO handle pagination
+     axios
+      .get(process.env.VUE_APP_WORDPRESS_REST_API+query)
+      .then(response => (this.posts = response.data))
+     }
+  },
+  created() {
+     document.title = process.env.VUE_APP_TITLE;
+     this.populatePosts();  
+  }, 
   components: {
-    HelloWorld
+    BookReview
   }
 }
 </script>
@@ -21,8 +79,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 20px;
 }
 </style>
---
+++ src/components/BookReview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{collections.first_letter}}
+    <ul>
+      <li v-for="book in collections.posts">
+        <a :href="book.link" v-html="book.title"></a>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookReview',
+  props: {
+    collections: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
